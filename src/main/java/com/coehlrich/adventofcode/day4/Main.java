0fbb866b2ed0a67243d31ea97b72dd5b54cf6d57
coehlrich <coehlrich@users.noreply.github.com>
2025-12-04 18:50:38 +1300

    Improve performance of day 4 solution

--- src/main/java/com/coehlrich/adventofcode/day4/Main.java
+++ src/main/java/com/coehlrich/adventofcode/day4/Main.java
@@ -16,23 +16,17 @@ public class Main implements Day {
             }
         }
 
-        int part1 = remove(map, false);
+        int[][] counts = count(map);
 
-        int runs = 1;
-        int removed = remove(map, true);
-        int part2 = 0;
-        while (removed != 0) {
-            runs++;
-            part2 += removed;
-            removed = remove(map, true);
-        }
+        int part1 = remove(counts, false);
 
-        System.out.println(runs);
+        int part2 = remove(counts, true);
+
         return new Result(part1, part2);
     }
 
-    public int remove(boolean[][] map, boolean modify) {
-        int result = 0;
+    public int[][] count(boolean[][] map) {
+        int[][] counts = new int[map.length][map[0].length];
         for (int y = 0; y < map.length; y++) {
             for (int x = 0; x < map[y].length; x++) {
                 if (map[y][x]) {
@@ -49,20 +43,50 @@ public class Main implements Day {
                             }
                         }
                     }
-                    if (count < 4) {
-                        result++;
-                        if (modify) {
-                            map[y][x] = false;
-                        }
-//                        System.out.print('x');
-                    } else {
-//                        System.out.print('@');
-                    }
+                    counts[y][x] = count;
                 } else {
-//                    System.out.print('.');
+                    counts[y][x] = -1;
                 }
             }
 //            System.out.println();
+        }
+        return counts;
+    }
+
+    public int remove(int[][] map, boolean modify) {
+        int result = 0;
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map[y].length; x++) {
+                if (map[y][x] >= 0 && map[y][x] <= 3) {
+                    if (modify) {
+                        result += check(map, x, y);
+                    } else {
+                        result++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public int check(int[][] map, int x, int y) {
+        int result = 0;
+        if (map[y][x] >= 0 && map[y][x] <= 3) {
+            map[y][x] = -1;
+            result = 1;
+            for (int dy = -1; dy <= 1; dy++) {
+                for (int dx = -1; dx <= 1; dx++) {
+                    if ((dy != 0 || dx != 0)
+                            && y + dy >= 0
+                            && x + dx >= 0
+                            && y + dy < map.length
+                            && x + dx < map[0].length
+                            && map[y + dy][x + dx] != -1) {
+                        map[y + dy][x + dx]--;
+                        result += check(map, x + dx, y + dy);
+                    }
+                }
+            }
         }
         return result;
     }
