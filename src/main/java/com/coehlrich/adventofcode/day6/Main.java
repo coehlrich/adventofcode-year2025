3a1cdfb283bcbd8a26f564f4a42b59838594921b
coehlrich <coehlrich@users.noreply.github.com>
2025-12-06 18:31:40 +1300

    Day 6 part 2

--- src/main/java/com/coehlrich/adventofcode/day6/Main.java
+++ src/main/java/com/coehlrich/adventofcode/day6/Main.java
@@ -2,7 +2,11 @@ package com.coehlrich.adventofcode.day6;
 
 import com.coehlrich.adventofcode.Day;
 import com.coehlrich.adventofcode.Result;
+import it.unimi.dsi.fastutil.longs.LongArrayList;
+import it.unimi.dsi.fastutil.longs.LongList;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Main implements Day {
@@ -15,12 +19,52 @@ public class Main implements Day {
                         .toArray(String[]::new))
                 .toArray(String[][]::new);
 
-        long total = 0;
+        List<Problem> part1 = new ArrayList<>();
         for (int x = 0; x < grid[0].length; x++) {
             char operation = grid[grid.length - 1][x].charAt(0);
-            long result = operation == '*' ? 1 : 0;
+            LongList values = new LongArrayList();
             for (int y = 0; y < grid.length - 1; y++) {
-                long value = Long.parseLong(grid[y][x]);
+                values.add(Long.parseLong(grid[y][x]));
+            }
+            part1.add(new Problem(values, operation));
+//            System.out.println(operation);
+        }
+
+        List<Problem> part2 = new ArrayList<>();
+        String[] lines = input.lines().toArray(String[]::new);
+        LongList currentValues = new LongArrayList();
+        char operation = '\0';
+        String operations = lines[lines.length - 1];
+        for (int i = 0; i < operations.length(); i++) {
+            if (operations.charAt(i) != ' ') {
+                if (operation != '\0') {
+                    part2.add(new Problem(currentValues, operation));
+                    currentValues = new LongArrayList();
+                }
+                operation = operations.charAt(i);
+            }
+            long number = 0;
+            for (int y = 0; y < lines.length - 1; y++) {
+                char character = lines[y].charAt(i);
+                if (character != ' ') {
+                    number *= 10;
+                    number += Character.digit(character, 10);
+                }
+            }
+            if (number != 0) {
+                currentValues.add(number);
+            }
+        }
+        part2.add(new Problem(currentValues, operation));
+        return new Result(calculate(part1), calculate(part2));
+    }
+
+    private long calculate(List<Problem> problems) {
+        long total = 0;
+        for (Problem problem : problems) {
+            char operation = problem.operation;
+            long result = operation == '*' ? 1 : 0;
+            for (long value : problem.values) {
                 result = switch (operation) {
                     case '*' -> result * value;
                     case '+' -> result + value;
@@ -30,7 +74,11 @@ public class Main implements Day {
             total += result;
 //            System.out.println(operation);
         }
-        return new Result(total, 0);
+        return total;
+    }
+
+    private record Problem(LongList values, char operation) {
+
     }
 
 }
