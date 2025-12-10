b54103516904f0bee368d2afc883e2d1a6b630db
coehlrich <coehlrich@users.noreply.github.com>
2025-12-10 20:10:14 +1300

    Current day 10 part 2

--- src/main/java/com/coehlrich/adventofcode/day10/Main.java
+++ src/main/java/com/coehlrich/adventofcode/day10/Main.java
@@ -2,11 +2,11 @@ package com.coehlrich.adventofcode.day10;
 
 import com.coehlrich.adventofcode.Day;
 import com.coehlrich.adventofcode.Result;
-import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
-import it.unimi.dsi.fastutil.ints.IntSet;
+import it.unimi.dsi.fastutil.ints.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,6 +20,7 @@ public class Main implements Day {
     public Result execute(String input) {
         List<Machine> machines = input.lines().map(Machine::parse).toList();
         int part1 = 0;
+        int part2 = 0;
         for (Machine machine : machines) {
             int min = Integer.MAX_VALUE;
             for (int i = 0; i < Math.pow(2, machine.buttons.size()); i++) {
@@ -39,11 +40,79 @@ public class Main implements Day {
                 }
             }
             part1 += min;
+            List<IntSet> buttons = machine.buttons.stream().sorted(Comparator.comparingInt(IntSet::size)).toList().reversed();
+            Int2ObjectMap<IntSet> finalButtons = new Int2ObjectOpenHashMap<>();
+            IntSet found = new IntOpenHashSet();
+            for (int i = buttons.size() - 1; i >= 0; i--) {
+                IntSet current = new IntOpenHashSet();
+                IntSet button = buttons.get(i);
+                for (int joltage : button) {
+                    if (found.add(joltage)) {
+                        current.add(joltage);
+                    }
+                }
+                finalButtons.put(i, current);
+            }
+            part2 += part2(machine, buttons, new int[machine.joltage.length], 0, 0, Integer.MAX_VALUE, finalButtons);
+//            System.out.println(part2);
+            System.out.println(machine);
         }
-        return new Result(part1, 0);
+        return new Result(part1, part2);
     }
 
-    public record Machine(boolean[] target, List<IntSet> buttons, IntSet joltage) {
+    public int part2(Machine machine, List<IntSet> buttons, int[] current, int presses, int i, int min, Int2ObjectMap<IntSet> finalButtons) {
+        if (presses > min) {
+            return min;
+        }
+        IntSet button = buttons.get(i);
+        int maxPresses = Integer.MAX_VALUE;
+        boolean next = buttons.size() - 1 > i;
+        IntSet required = finalButtons.get(i);
+        int expected = -1;
+        current = Arrays.copyOf(current, current.length);
+        for (int joltage : button) {
+            if (required.contains(joltage)) {
+                if (expected == -1) {
+                    expected = machine.joltage[joltage] - current[joltage];
+                } else if (expected != machine.joltage[joltage] - current[joltage]) {
+                    return Integer.MAX_VALUE;
+                }
+            } else {
+                maxPresses = Math.min(maxPresses, machine.joltage[joltage] - current[joltage]);
+            }
+        }
+
+        if (expected != -1) {
+            for (int joltage : button) {
+                current[joltage] += expected;
+            }
+            if (expected > maxPresses) {
+                return Integer.MAX_VALUE;
+            } else if (next) {
+                return part2(machine, buttons, current, presses + expected, i + 1, min, finalButtons);
+            } else {
+                return presses + expected;
+            }
+        }
+
+        for (int j = 0; j <= maxPresses; j++) {
+            if (j > 0) {
+                presses++;
+                if (presses > min) {
+                    return min;
+                }
+                for (int joltage : button) {
+                    current[joltage]++;
+                }
+            }
+            if (next) {
+                min = Math.min(min, part2(machine, buttons, current, presses, i + 1, min, finalButtons));
+            }
+        }
+        return min;
+    }
+
+    public record Machine(boolean[] target, List<IntSet> buttons, int[] joltage) {
         public static Machine parse(String line) {
             Matcher matcher = PATTERN.matcher(line);
             matcher.matches();
@@ -62,10 +131,9 @@ public class Main implements Day {
                                 .toArray()));
             }
 
-            IntSet joltage = new IntOpenHashSet(
-                    Stream.of(matcher.group(3).split(","))
-                            .mapToInt(Integer::parseInt)
-                            .toArray());
+            int[] joltage = Stream.of(matcher.group(3).split(","))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
             return new Machine(target, buttons, joltage);
         }
     }
