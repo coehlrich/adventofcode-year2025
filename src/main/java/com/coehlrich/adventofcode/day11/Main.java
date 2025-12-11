81f3c55c1d403de9ce06b6fd3d3b4d977a3b562f
coehlrich <coehlrich@users.noreply.github.com>
2025-12-11 18:20:04 +1300

    Day 11 part 2

--- src/main/java/com/coehlrich/adventofcode/day11/Main.java
+++ src/main/java/com/coehlrich/adventofcode/day11/Main.java
@@ -2,8 +2,8 @@ package com.coehlrich.adventofcode.day11;
 
 import com.coehlrich.adventofcode.Day;
 import com.coehlrich.adventofcode.Result;
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2LongMap;
+import it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,25 +23,36 @@ public class Main implements Day {
             connections.put(device, outputs);
         }
 
-        Object2IntMap<String> cache = new Object2IntOpenHashMap<>();
-
-        return new Result(getPaths("you", cache, connections), 0);
+        return new Result(getPaths("you", new Object2LongOpenHashMap<>(), connections, true, true), getPaths("svr", new Object2LongOpenHashMap<>(), connections, false, false));
     }
 
-    public int getPaths(String value, Object2IntMap<String> cache, Map<String, Set<String>> connections) {
+    public long getPaths(String value, Object2LongMap<State> cache, Map<String, Set<String>> connections, boolean dac, boolean fft) {
+        State cacheKey = new State(value, dac, fft);
         if (value.equals("out")) {
-            return 1;
-        } else if (cache.containsKey(value)) {
-            return cache.getInt(value);
+            if (dac && fft) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else if (cache.containsKey(cacheKey)) {
+            return cache.getLong(cacheKey);
         }
-        int count = 0;
+        if (!dac && value.equals("dac")) {
+            dac = true;
+        } else if (!fft && value.equals("fft")) {
+            fft = true;
+        }
+        long count = 0;
         if (connections.containsKey(value)) {
             for (String output : connections.get(value)) {
-                count += getPaths(output, cache, connections);
+                count += getPaths(output, cache, connections, dac, fft);
             }
         }
-        cache.put(value, count);
+        cache.put(cacheKey, count);
         return count;
+    }
+
+    public record State(String value, boolean dac, boolean fft) {
     }
 
 }
